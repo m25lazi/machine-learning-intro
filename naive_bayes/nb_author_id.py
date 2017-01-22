657e7301e5d6e76c04abc7a2ad2ac0e20fd1888b
Lazim Mohammed <mlazim@vmware.com>
2017-01-22 15:25:19 +0530

    Naive Bayes Accuracy
    Author identification using gaussian nb from sklearn

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,6 +27,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print accuracy;
+
+# Training time: 2.036 s
+# Prediction time: 0.184 s
+# 0.973265073948
 
 #########################################################
 
